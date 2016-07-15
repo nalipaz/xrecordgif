f552c18bb4c9dda75620361298d588ea8256cb65
Nicholas Alipaz <nalipaz@labiomed.org>
2016-07-14 17:59:20 -0700

    Rename c file to reduce any confusion

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm ~/bin/xrecordgif -f
-gcc xrectsel.c -lX11 -o ~/bin/xrecordgif `pkg-config --cflags --libs libnotify`
+gcc xrecordgif.c -lX11 -o ~/bin/xrecordgif `pkg-config --cflags --libs libnotify`
 chmod +x ~/bin/xrecordgif
